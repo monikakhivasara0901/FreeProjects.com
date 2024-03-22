95456f99987039ae6385292900257b8e5c779bd7
Radhey-R-Kedar <radheykedar28@gmail.com>
2024-03-22 19:43:58 +0530

    mongodb uri

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        await mongoose.connect(process.env.MONGO_URI!);
+        // await mongoose.connect(process.env.MONGO_URI!);
+        await mongoose.connect("ji");
         const connection = mongoose.connection;
         connection.on("connected", () => {
             console.log("MongoDB connected successfully");
