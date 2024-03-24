95bedb3bd13b76ccbf9ef02a12c3a16fca77d52f
Radhey-R-Kedar <radheykedar28@gmail.com>
2024-03-24 19:18:07 +0530

    Working

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,20 @@
+"use client";
 import { NextPage } from "next";
 import NavBar from "../components/NavBar";
 import ProjectTopContainer from "../components/ProjectTopContainer";
 import FilterBox from "../components/FilterBox";
 import ProjectViewContainer from "../components/ProjectViewContainer";
+import { useState } from "react";
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
+  const [loginSignUp, setLoginSignup] = useState(false);
+  const [type, setType] = useState("login");
+  
   return (
     <div className="flex flex-col flex-1  h-[110vh]">
-      <NavBar />
+      <NavBar setLoginSignup={setLoginSignup} setType={setType} />
       <ProjectTopContainer />
       <div className="flex h-[100%] flex-row">
         <FilterBox />
